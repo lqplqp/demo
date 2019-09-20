ca14efc81c23a2428630fd9a024542061e0c9049
lqp <133566770@qq.com>
2019-09-20 19:12:39 +0800

    特征选择完成 开始建立模型

---
+++ model/Model.py
@@ -0,0 +1,8 @@
+# import numpy as np
+# import tensorflow as tf
+#
+# from utils.data_util import futures_data
+#
+#
+# class Model:
+#     def interface(futures_data , ):
--- utils/data_util.py
+++ utils/data_util.py
@@ -3,29 +3,37 @@ import math
 import os
 import random
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 random.seed(666)
 np.random.seed(666)
 
 PATH = "..//train_set/"
+# 选取最大文件数
 SELECT_FILES = 100
+# 最大文件数
 MAX_FILES = 4000
+# 方差值小于15的特征会被剔除
+LITTLE_VARIANCE = 15
 
 
 # 加载csv数据
 def load_csv_file(path):
-    return remove_head(np.loadtxt(path,delimiter=",",dtype=str))
+    return remove_head(np.loadtxt(path, delimiter=",", dtype=str))
+
 
 # 移除表头
 def remove_head(data):
     return data[1:, :]
 
+
 # 随机读取200个文件到内存中
 def read_resoures():
     files = os.listdir(PATH)
-    random_index = np.random.randint(0,MAX_FILES,SELECT_FILES)
+    random_index = np.random.randint(0, MAX_FILES, SELECT_FILES)
     files = np.array(files)
     return files[random_index]
+
 
 # 把获取的数据读取到data中[200,2291,18]
 def load_memory(file_names):
@@ -41,25 +49,23 @@ def load_memory(file_names):
                 result.append(i3)
 
     result = np.array(result)
-    result = result.reshape(-1,18)
+    result = result.reshape(-1, 18)
 
     return result
-
 
 
 # 随机
 data = load_memory(read_resoures())
 
-
 print(len(data))
 
-label_data = data[:,-1]
-futures_data = data[:,0:-1]
+label_data = data[:, -1]
+futures_data = data[:, 0:-1]
 # print(label_data.shape) # 标签形状 (2861,)
 # print(futures_data.shape) # 数据形状(2861,17)
 
 futures_data = futures_data.astype(np.float64)
-print(futures_data.shape) # (2861, 17)
+print(futures_data.shape)  # (2861, 17)
 
 
 # hv 是栅格与发射机的距离d以及栅格与信号线的相对高度
@@ -71,46 +77,53 @@ print(futures_data.shape) # (2861, 17)
 # 2 position_y 发射机所在y坐标
 # 12 x 目标所在x
 # 13 y 目标所在y
-def culter_hv(h_b,sit_md,sit_ed,position_x,position_y,x,y):
-
-    result_x = np.power(x-position_x,2)
-    result_y = np.power(y-position_y,2)
-    d = np.sqrt(result_x+result_y)
+def culter_hv(h_b, sit_md, sit_ed, position_x, position_y, x, y):
+    result_x = np.power(x - position_x, 2)
+    result_y = np.power(y - position_y, 2)
+    d = np.sqrt(result_x + result_y)
 
     tan = np.tan(np.radians(sit_md + sit_ed))
-    h_v = h_b - tan*d
+    h_v = h_b - tan * d
     return h_v
+
 
 # 计算hvs 返回的是(2861,)
 def culter_hvs(futures_data):
-    return culter_hv(futures_data[:,3],futures_data[:,6],futures_data[:,5],futures_data[:,1],futures_data[:,2],futures_data[:,12],futures_data[:,13])
+    return culter_hv(futures_data[:, 3], futures_data[:, 6], futures_data[:, 5], futures_data[:, 1], futures_data[:, 2],
+                     futures_data[:, 12], futures_data[:, 13])
 
-hv = culter_hvs(futures_data) # (2861,)
 
-futures_data = np.delete(futures_data,[5,6,1,2,12,13],axis=1) # (2861, 11)
+hv = culter_hvs(futures_data)  # (2861,)
 
-#
-futures_data = np.c_[futures_data , hv] # (2861, 12)
+futures_data = np.delete(futures_data, [ 5, 6, 1, 2, 12, 13], axis=1)  # (2861, 10)
 
-# 超参数
-mean_futures = np.mean(futures_data , axis=0)
+# 融入特征hv
+futures_data = np.c_[futures_data, hv]  # (2861, 11)
+
 
 # 方差
-a = futures_data - mean_futures
-b = np.power(a , 2)
-c = np.sum(b,axis=0)
-d = c/len(futures_data)
-print(d)
+def variance(futures_data):
+    mean_futures = np.mean(futures_data, axis=0)
+    a = futures_data - mean_futures
+    b = np.power(a, 2)
+    c = np.sum(b, axis=0)
+    d = c / len(futures_data)
+    return d
 
-f = futures_data[:,1]
 
-def Normalize(f):
-    return np.divide(f , f.sum())
+# 获取小于阈值的index
+def getIndex(vari):
+    size = vari.size
+    indexs = []
+    for index in range(size):
+        if (vari[index] < LITTLE_VARIANCE):
+            indexs.append(index)
+    return indexs
 
-# f = Normalize(f)
 
-plt.scatter(f,f)
-plt.show()
+vari = variance(futures_data) # (11,)
+delete_index = getIndex(vari)
 
-# variance = np.divide(np.sum(np.power((futures_data - mean_futures) , 2)) , len(futures_data))
-# print(variance)
+futures_data = np.delete(futures_data, delete_index, axis=1) # (*,9)
+
+
